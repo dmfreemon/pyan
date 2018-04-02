62fe89e1d6e715820f23f3257c7c3c479961af1e
dmfreemon@users.noreply.github.com <dmfreemon@users.noreply.github.com>
2018-04-02 11:41:38 -0500

    minor doc update

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# runs under cygwin; should work for any unix-like O/S
+#
+# python3 pyan.py --help
+
 python3 -u -OO pyan.py --dirlist ~/pyan-dmfreemon/dirs-test-tiny.txt --uses --no-defines --colored --nested-groups --yed --file /cygdrive/c/junk/outfile.graphml
 
